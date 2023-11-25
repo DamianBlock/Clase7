ca29693de7bd740d19724c1bf51d400c88ccb8ef
DamianBlock <damianfrt25@gmail.com>
2023-11-24 22:23:56 -0300

    getter and setters

--- src/main/java/org/example/Usuario.java
+++ src/main/java/org/example/Usuario.java
@@ -4,6 +4,40 @@ public class Usuario {
     Long id;
     String usuario;
     String nombre;
-
     int edad;
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
